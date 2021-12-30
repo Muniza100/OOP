094569d51370869a047e011331385381ffb07730
Muniza Hashim <90342558+Muniza100@users.noreply.github.com>
2021-12-30 12:26:39 +0600

    Add files via upload

---
+++ ProblemSet7/Customer.java
@@ -0,0 +1,30 @@
+public class Customer {
+    private final int ID;
+    private final String name;
+    private int discount;
+
+    public Customer(int ID, String name, int discount) {
+        this.ID = ID;
+        this.name = name;
+        this.discount = discount;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public String toString() {
+        return name + '(' + ID + ')';
+    }
+}
---
+++ ProblemSet7/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        MyTime test = new MyTime(1, 3, 64);
+        System.out.println(test.nextSecond());
+    }
+}
