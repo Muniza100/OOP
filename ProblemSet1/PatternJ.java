665e10a2ac4c62836553dcaba9b1f61789815638
Muniza Hashim <90342558+Muniza100@users.noreply.github.com>
2021-12-30 12:19:48 +0600

    Add files via upload

---
+++ ProblemSet1/PatternJ.java
@@ -0,0 +1,13 @@
+public class PatternJ {
+    public static void main(String[] args) {
+        System.out.println("---**********-");
+        System.out.println("-------**-----");
+        System.out.println("-------**-----");
+        System.out.println("-------**-----");
+        System.out.println("-------**-----");
+        System.out.println("**-----**-----");
+        System.out.println("**----**------");
+        System.out.println("-**--**-------");
+        System.out.println("--****--------");
+    }
+}
---
+++ ProblemSet1/Victory.java
@@ -0,0 +1,7 @@
+public class Victory {
+    public static void main(String[] args) {
+        System.out.print("//////////////////////\n" +
+                "|| Victory is mine! ||\n" +
+                "\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
