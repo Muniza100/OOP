fa2263d3b031babe0de4b4eb43fd396319b1320a
Muniza Hashim <90342558+Muniza100@users.noreply.github.com>
2021-12-30 12:21:43 +0600

    Add files via upload

---
+++ ProblemSet3/StarSquare.java
@@ -0,0 +1,10 @@
+public class StarSquare {
+    public static void main(String[] args) {
+        String line = "*****";
+        int lineCounter = 0;
+        while (lineCounter < 4) {
+            System.out.println(line);
+            lineCounter++;
+        }
+    }
+}
